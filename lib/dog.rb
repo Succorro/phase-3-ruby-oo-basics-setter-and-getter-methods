d86326807568c6ab4b8e7fab18ec44645fb4efdc
Steven Gutierrez <stevogbmv@gmail.com>
2023-05-09 13:38:23 -0700

    commit tree 86a65ed508aefb4c3344318c485897d35929aec3

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,8 @@
 class Dog
-
+    attr_accessor :name
+    attr_accessor :breed 
+  def initialize(name = 'Joe', breed = 'Mastiff')
+    @name = name
+    @breed = breed
+  end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-
+    attr_accessor :name
+    attr_accessor :job
+    def initialize(name = "Bri", job = 'Marketing Consultant')
+      @name = name
+      @job = job
+    end
 end
